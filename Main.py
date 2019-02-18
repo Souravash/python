81d0d2300b94381c4d6cb06049e3d393557104b9
Souravash <47749620+Souravash@users.noreply.github.com>
2019-02-18 21:10:28 +0530

    Add files via upload
    #variables_homework1

---
+++ Main.py
@@ -0,0 +1,18 @@
+#my first python assignment on  variables
+#my first  home work
+
+my_song = "Jai ho" 
+album_name ="Slum dog millionare"
+genre="Jazz" #its a string
+composer="A R Rahman"
+duration= 5.19 #its a float 
+total_tracks =13  #its a int
+""" this is an oscar award winning song"""
+print(my_song)
+print(album_name)
+print(genre)
+print(composer)
+print(duration)
+print(total_tracks)
+
+
