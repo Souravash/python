26c955b8e5028f9bb99e22db289fdddfd7d62905
Souravash <47749620+Souravash@users.noreply.github.com>
2019-02-19 09:12:56 +0530

    Add files via upload

---
+++ func.py
@@ -0,0 +1,18 @@
+def Artist():
+	name="A R rahman"
+	return name
+def genre():
+	type="jazz"
+	return type
+def release():
+	year=2008
+	return year
+def oscarWinning():
+        if (winning== "True"):
+                return True
+        else:
+                return False
+Artist()
+genre()
+release()
+
